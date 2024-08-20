aa49256ba68e541ee366e0230250d802925e66ab
santipt <sanpetor@epsg.upv.es>
2024-08-20 13:29:58 +0200

    Using the redux states & Minor comments

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,27 @@
 import React, { useEffect } from 'react';
-import { services } from '../api/services';
+import { fetchProducts } from '../redux/slices/productsListSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState, AppDispatch } from '../redux/store';
 
 function Home() {
+    const dispatch = useDispatch<AppDispatch>();
+    const { products, status, error } = useSelector((state: RootState) => state.products);
 
     useEffect(() => {
-        services?.fetchProductDetails();
-    }, []);
+        if (status === 'idle') {
+            dispatch(fetchProducts());
+        }
+    }, [dispatch, status]);
 
     return (
         <div>
+            {status === 'loading' && <p>Loading...</p>}
+            {status === 'failed' && <p>Error: {error}</p>}
+            {status === 'succeeded' && (
+                products.map((product : any) => (
+                    <p key={product?.fmyEngName}>{product?.fmyEngName}</p>
+                ))
+            )}
         </div>
     );
 }
--- src/types/models.ts
+++ src/types/models.ts
@@ -1,7 +1,12 @@
 // Defining global types used throughout the application to ensure consistency
+
+// Because productList is a long list of jsons
+// I'm using 'any' which I'm aware that is a bad practice. 
+// I would define each property in the Product interface in a real environment
 export interface Product {
     familyId: string;
     fmyMarketingName: string;
+    // All the other properties...
 }
 
 export interface ApiResponse {
@@ -10,7 +15,7 @@ export interface ApiResponse {
         statusMessage: string;
         siteCode: string;
         resultData: {
-            productList: any[];
+            productList: Product[] | any;
         };
     }
 }
